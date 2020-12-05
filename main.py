14483af176f93a9328e0031a1ad959565300bb3c
GU-U7 <grover.ugarte@utec.edu.pe>
2020-12-04 20:47:26 -0500

    importar Proyecto 1

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Grupo P1 3
+Grover Eduardo Ugarte Quispe 202020159
+Daniel Arturo Ventura Atarama 202010429
+"""
+
+import funcMain
+import os
+
+def main():
+
+    os.system("clear")
+
+    controlAbandondo=[0,0,0]
+    intentos = [5]
+
+    funcMain.menuPrincipal(controlAbandondo, intentos)
+
+    os.system("clear")
+
+main()
